9e6b4ebd7a3f6b9475f9e20b00c9dae13411af08
Meg <meg_gauthier@yahoo.com>
2017-02-27 19:31:12 -0600

    insert_sample_question is complete.

---
+++ insert_sample_question.php
@@ -0,0 +1,22 @@
+<?php
+
+function insert_sample_question() {
+	global $wpdb;
+	
+	$user_id = 1;
+	$question = 'How many gnomes does it take to answer a question?';
+	
+	$table_name = $wpdb->prefix . 'gs_question';
+	
+	$wpdb->insert( 
+		$table_name, 
+		array( 
+			'user_id' => $user_id, 
+			'question' => $question
+		) 
+	);
+}
+
+
+
+?>
