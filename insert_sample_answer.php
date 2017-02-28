97dbfe2ffd1968b0a4631fd3a944cba5722c9a65
Caleb <lakefeb2345@gmail.com>
2017-02-27 19:02:53 -0600

    started insert_sample_answer.php

---
+++ insert_sample_answer.php
@@ -0,0 +1,15 @@
+<?php
+
+global $gs_db_version;
+$gs_db_version = '1.0';
+
+function insert_sample_answer() {
+    global $wpdb;
+    
+    $user_id = 1;
+    $guestion_id = 1;
+    $answer = 'This is a sample answer.';
+    $upvote = 0;
+    
+    $table_name = 
+}
