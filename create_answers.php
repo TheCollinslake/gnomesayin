082c3d7da393750dd7c8039b073d8d5332a057e4
Kevin <kevin.firminmsl@gmail.com>
2017-03-01 17:24:42 -0600

    Changed the name of the table

--- create_answers.php
+++ create_answers.php
@@ -7,17 +7,17 @@
  */
 function answers_install_db() {
     global $wpdb;
-    $table_name = $wpdb->prefix . 'create_answers';
+    $table_name = $wpdb->prefix . 'gs_answer';
 
         $sql = "CREATE TABLE $table_name (
-
                   UNIQUE KEY id (id),
                   user_id mediumint(9) NOT NULL FOREIGN ,
                   answer text NOT NULL,
                   time_stamp DATETIME_INTERVAL_CODE AUTO CURRENT_TIMESTAMP,
                   upvote mediumint(9),
                   question_id mediumint(9) FOREIGN NOT NULL 
-      
     );";
 
     }
+
+
