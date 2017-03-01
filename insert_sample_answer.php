2eba116338c708c2d37d690ef8ddb80aadd8c2c6
Caleb <lakefeb2345@gmail.com>
2017-03-01 17:25:16 -0600

    updated insert_sample_answer.php

--- insert_sample_answer.php
+++ insert_sample_answer.php
@@ -7,16 +7,16 @@ function insert_sample_answer() {
     global $wpdb;
     
     $user_id = 1;
-    $guestion_id = 1;
+    $question_id = 1;
     $answer = 'This is a sample answer.';
     $upvote = 0;
     
-    $table_name = $wpdb->perefix . 'gs_ansers'; /*dont know if this is right*/ 
+    $table_name = $wpdb->perefix . 'gs_answer'; /*dont know if this is right*/ 
     
     wpdb->insert(
         $table_name, array(
             'user_id'=> $user_id,
-            'guestion_id'=> $guestion_id,
+            'question_id'=> $question_id,
             'answer'=> $answer,
             'upvote'=> $upvote
         )
